e16ca4d3e16f4e632efcc663914a9444c839de6a
Afshin Khadangi <39042320+akhadangi@users.noreply.github.com>
2025-02-03 12:37:31 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Example: Dynamically resize iframe based on content
-window.addEventListener('message', function(event) {
-    const iframe = document.querySelector('iframe');
-    if (event.data.height) {
-        iframe.style.height = event.data.height + 'px';
-    }
-});
